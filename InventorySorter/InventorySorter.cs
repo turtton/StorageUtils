96339c52473dfb089cc366dab042d9116a54d70c
turtton <watagame2579@gmail.com>
2021-03-07 21:05:59 +0900

    :hammer: add middle click sorting

--- InventorySorter/InventorySorter.cs
+++ InventorySorter/InventorySorter.cs
@@ -2,6 +2,7 @@
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using UnityEngine;
 
 namespace InventorySorter {
     [BepInPlugin("net.github.turtton.plugins.inventorysorter", "Inventory Sorter", "1.0.0")]
@@ -18,6 +19,33 @@ namespace InventorySorter {
 
         private void OnDestroy() {
             _harmony.UnpatchAll();
+        }
+
+        private readonly FieldInfo _containerGridField = typeof(InventoryGui)
+            .GetField("m_containerGrid", BindingFlags.Instance | BindingFlags.GetField | BindingFlags.NonPublic);
+        private readonly FieldInfo _playerGridField = typeof(InventoryGui)
+            .GetField("m_playerGrid", BindingFlags.Instance | BindingFlags.GetField | BindingFlags.NonPublic);
+
+        private readonly FieldInfo _animatorField = typeof(InventoryGui)
+            .GetField("m_animator", BindingFlags.Instance | BindingFlags.GetField | BindingFlags.NonPublic);
+
+        private static readonly int Visible = Animator.StringToHash("visible");
+
+        private void Update() {
+            var instance = InventoryGui.instance;
+            if (instance is null) return;
+            
+            
+            var animator = (Animator) _animatorField?.GetValue(instance);
+            if (!(animator is null) && (!Input.GetMouseButtonDown(2) || !animator.GetBool(Visible))) return;
+
+            InventoryGrid inventoryGrid;
+            if (instance?.IsContainerOpen() == true) {
+                inventoryGrid = (InventoryGrid) _containerGridField?.GetValue(instance);
+            } else {
+                inventoryGrid = (InventoryGrid) _playerGridField?.GetValue(instance);
+            }
+            if (!(inventoryGrid is null)) InventoryUtils.SortInventory(inventoryGrid.GetInventory());
         }
     }
 }
