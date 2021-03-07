476161bef8558c8b16a0ce7f1edc106a26e6960d
turtton <watagame2579@gmail.com>
2021-03-07 21:04:22 +0900

    :hammer: Create `Inventory$Changed()` accessor

---
+++ InventorySorter/patches/InventoryPatch.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace InventorySorter.patches {
+    [HarmonyPatch]
+    public class InventoryAccessor {
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(Inventory), "Changed")]
+        public static void Changed(object instance) {}
+    }
+}
